96214812887f07e8b002f7cdc403438aab7bceb7
Hariharanreddy <animatrix536@gmail.com>
2023-12-11 18:27:17 +0530

    sidebar implemented

---
+++ dashboard/src/theme/theme.js
@@ -0,0 +1,12 @@
+"use client";
+
+// In a file like theme.js
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Inter, sans-serif", // Use 'Inter' as the default font
+  },
+});
+
+export default theme;
