dcc66dda7b64bb3c8c341c4d9ef81b7f06933b18
yogesh dubey <yogesh.dubey.0804@gmail.com>
2023-12-12 07:03:38 +0530

    fix : dependencies

--- dashboard/src/components/ESGTabs/index.js
+++
@@ -1,9 +0,0 @@
-import EnvironmentInitiative from "./EnvironmentInitiative";
-import SocialInitiative from "./SocialInitiative";
-import GovernanceInitiative from "./GovernanceInitiative";
-
-export {
-    EnvironmentInitiative,
-    SocialInitiative,
-    GovernanceInitiative
-}
--- packages/esg-sdk/dist/src/methods/get_file_content/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { FileContent } from "../../models/file_content";
-/**
- * Fetches a list of FileContent instances from the specified API endpoint.
- * @param {string} dir - The directory parameter, Must be one of environment, social, governance.
- * @param {string} fileName - The name of the file without extension.
- * @returns {Promise<FileContent>} - A Promise that resolves to an array of FileContent instances.
- */
-export default function getFileContent(dir: string, fileName: string): Promise<FileContent>;
-//# sourceMappingURL=index.d.ts.map
