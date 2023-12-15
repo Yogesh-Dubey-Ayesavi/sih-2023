e7a97d64ecbf9f11834cea2a51381b7e2c33a393
yogesh dubey <yogesh.dubey.0804@gmail.com>
2023-12-15 12:24:18 +0530

    fix:ignore broken links

--- client/docusaurus.config.js
+++ client/docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
   organizationName: 'Ayesavi', // Usually your GitHub org/user name.
   projectName: 'sih-template', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
